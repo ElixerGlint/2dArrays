69c04762d9d23e272c711ed1fd3d185f1ce7c61c
Elixer <everett.rumery@gmail.com>
2024-12-04 13:43:20 -0500

    bazinga

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[][] ard2d;
-        Book[] shelf;
+        int[][] arr2d = new int[3][4];
+        for(int i = 0; i < arr2d.length; i++) {
+            for(int j = 0; j < arr2d[i].length; j++) {
+
+            }
+        }
     }
 }
